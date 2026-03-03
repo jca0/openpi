48ffbbe0fe308ac69dc86ac78995ecc2404e51ca
Jing Cao <jcao0715@gmail.com>
2026-03-03 17:27:27 +0000

    new finetuning config

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -904,7 +904,7 @@ _CONFIGS = [
         ),
         data=LeRobotDROIDDataConfig(
             # Replace with your custom DROID LeRobot dataset repo id.
-            repo_id="your_hf_username/my_droid_dataset",
+            repo_id="sim/droid",
             base_config=DataConfig(prompt_from_task=True),
             assets=AssetsConfig(
                 # Important: reuse the original DROID norm stats during fine-tuning!
